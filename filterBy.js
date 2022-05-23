9b24605cfc39c4039cef995800636819449d8405
Selin Su Varol <selinsuvarol@gmail.com>
2022-05-23 16:44:13 +0300

    added specific release date filter function

--- filterBy.js
+++ filterBy.js
@@ -14,3 +14,24 @@ const filterByFetch = async (filepath) => {
     const res = await fetch(url);
     return res.json();
   };
+
+// regular filters end here and release date filter starts here 
+const filterByReleaseDate = async () => {
+    cleaner();
+  const movies = await filterByReleaseDateFetch();
+  renderMovies(movies.results);
+};
+
+const filterByReleaseDateFetch = async () => {
+    const url = releaseDateUrlGenerator();
+    const res = await fetch(url);
+    return res.json();
+  };
+
+const releaseDateUrlGenerator = () => {
+    return `https://api.themoviedb.org/3/discover/movie?api_key=${atob("NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=")}&sort_by=release_date.desc`;
+};
+
+// static _ReleaseDataConstructUrl() {
+//   return `${this.TMDB_BASE_URL}/discover/movie?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}&sort_by=release_date.desc`;
+// }
